eca934e8b07aa07bdc20c9cb8a6ce0622578aff1
eddie leftie <99412054+uwishema11@users.noreply.github.com>
2022-03-29 11:06:14 +0200

    file renaming

--- arrayAndObjects.arrayFormatted.js
+++ arrayAndObjects.arrayFormatted.js
@@ -1,10 +1,9 @@
-const array = [1,2,3];
-const newArray = [];
-let reverseArray =(array) => {
-    for(let a= array.length-1; a >=0 ;a--){
-        newArray.push(array[a]);
-    }
-    return newArray;
+const array = ["Patrick wyne, 30", "lil wyne, 32","Eric mimi, 21","Dodos deck, 21","Alian Dwine, 22","Patrick wyne, 33","Patrick wyne, 100","Patrick wyne, 40"];
+const nestedObject = {};
+
+let objectFunction =(array) => { 
+    array.map(el =>nestedObject[el.split(" ")[0]]= { second_name: el.split(" ")[1].slice(0,-1), age: el.split(" ")[2]} )
+    return nestedObject;
 };
 
-console.log(reverseArray(array));
+console.log(objectFunction(array));
