3a1e5759e828928aa1c167f133547aafccceb556
eddie leftie <59047760+uwambajeddy@users.noreply.github.com>
2022-03-31 04:55:44 +0200

    Update arrayAndObjects.prime.js

--- arrayAndObjects.prime.js
+++ arrayAndObjects.prime.js
@@ -1,11 +1,32 @@
 
-const primeArray= [1,2,3,4,5,6,7,8,9,35,36,37,38,39,41];
-let primeSort = (array) => {
+const primeArray= [1,2,3,4,5,6,7,8,9,35,36,37,38,39,41,500000000];
+/* let primeSort = (array) => {
     let primeNumber = [], isPrime = true;
     let newArray = array.filter( el => el >1 && el !== 1);
 newArray.map(el =>{
     for(let a=2; a < el;a++){
         el % a == 0 ? isPrime = false : isPrime;  
+    }
+    isPrime ? primeNumber.push(el) : "";
+    isPrime = true;
+});
+
+return primeNumber;
+}
+
+
+console.log(primeSort(primeArray)); */
+
+
+
+let primeSort = (array) => {
+    let primeNumber = [], isPrime = true;
+    array.filter( el => el >1 ).map(el =>{
+    for(let a=2; a < el;a++){
+       if( el % a == 0){
+        isPrime = false;
+        break;
+       };  
     }
     isPrime ? primeNumber.push(el) : "";
     isPrime = true;
