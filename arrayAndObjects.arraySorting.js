0fc039ace2e59d9cd2a8585f060fc50331c4d666
eddie leftie <59047760+uwambajeddy@users.noreply.github.com>
2022-03-31 18:36:11 +0200

    fix desc sorting

--- arrayAndObjects.arraySorting.js
+++ arrayAndObjects.arraySorting.js
@@ -1,53 +1,74 @@
 const array = [1,2,10,4,7,8,56,9,10,17,13,11];
 
 function sorter(array) {
+
+  //sorting array in ascending order
   const newArray = [];
   let min = array[0],max = array[0],pos = 0;
-
-   for(let j=0; j< array.length;j++){
-     if(max < array[j]){
-       max = array[j];
+  
+  //find the maxmum number
+  for(let j=0; j< array.length;j++){
+    if(max < array[j]){
+      max = array[j];
      }
-   }
+    }
+   /*  --------------------- */
    
-   for(let b =0; b <  array.length; b++) {
-     
-     for(let a=0; a< array.length;a++){
-       if(array[a] !== null){
-
-         if(min > array[a]){
-           min = array[a];
+   //loop that will help to srot array
+    for(let b =0; b <  array.length; b++) {
+      
+      //find the minmum number
+      for(let a=0; a< array.length;a++){
+      if(array[a] !== null){
+          
+          if(min > array[a]){
+            min = array[a];
            pos = a;
           }
           
         }
-     }
-
+      }
+      /*  --------------------- */
+     
      array[pos] = null;
      newArray[b] = min;
      min = max;
-  }
-  
-   
-
-    let primeSort = (array) => {
-    let primeNumber = [], isPrime = false;
-    array.filter( el => el >1 ).map(el =>{
-      if(el === 2) isPrime = true;
-    for(let a=2; a < el;a++){
-      
-       if( el % a == 0){
-        isPrime = true;
-        break;
-       };  
     }
-    isPrime ? primeNumber.push(el) : "";
-    isPrime = false;
+
+    /* ------------------------------------- */
+    
+    
+   //removing prime number 
+    let primeSort = (array) => {
+      let primeNumber = [], isPrime = false;
+    array.filter( el => el >1 ).map(el =>{
+      if(el === 2) isPrime = false;
+      for(let a=2; a < el;a++){
+        
+        if( el % a == 0){
+          isPrime = true;
+          break;
+        };  
+      }
+      isPrime ? primeNumber.push(el) : "";
+      isPrime = false;
 });
 
 return primeNumber;
 }
-return primeSort(newArray);
-  }
+/* ------------------------------------- */
 
-console.log(  sorter(array));
+//sorting in descinding order
+
+const primeArray =  primeSort(newArray);
+const descArray = [];
+primeArray.map(el=> descArray.unshift(el));
+/* ------------------------------------- */
+
+ return descArray;
+
+ 
+}
+
+
+console.log( sorter(array) );
--- arrayAndObjects.prime.js
+++ arrayAndObjects.prime.js
@@ -1,10 +1,13 @@
 
-const primeArray= [1,2,3,4,5,6,7,8,9,35,36,37,38,39,41,500000000];
+const primeArray= [1,2,2,3,4,5,6,7,8,9,35,36,37,38,39,41,500000000];
+
 /* let primeSort = (array) => {
     let primeNumber = [], isPrime = true;
-    let newArray = array.filter( el => el >1 && el !== 1);
-newArray.map(el =>{
+    let newArray = array.filter( el => el >1);
+
+    newArray.map(el =>{
     for(let a=2; a < el;a++){
+
         el % a == 0 ? isPrime = false : isPrime;  
     }
     isPrime ? primeNumber.push(el) : "";
